a6413c217a25c83f5932bc538560cec2fa97e385
Joshua Holbrook <josh.holbrook@gmail.com>
2011-10-05 20:53:04 -0700

    Now with sweet templates!

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
 var util = require("util"),
-    http = require("http");
+    http = require("http"),
+    ejs = require("ejs"),
+    static = require("node-static"),
+    fs = require("fs");
+
+var getIp = function (req) {
+  return {
+    ip: ( req.headers["X-Forwarded-For"]
+      || req.headers["x-forwarded-for"]
+      || req.client.remoteAddress )
+  };
+};
 
 http.createServer(function (req, res) {
-    var headers = JSON.stringify(req.headers, true);
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.write("Client Remote Address: "+req.client.remoteAddress+'\n');
-    res.write("HTTP Header X-Forwarded-For: "+headers["X-Forwarded-For"]+'\n');
-    res.end("Connection Remote Address: "+req.connection.remoteAddress);
-}).listen(1337);
+  res.writeHead(200, {"Content-Type": "text/html"});
+
+  if (req.url == "/") {
+    var ip = getIp(req);
+    console.log(ip);
+
+    fs.readFile(__dirname+"/templates/index.html", function (err, template) {
+      if (err) {
+          throw err;
+      }
+
+      res.end(ejs.render(template.toString(), { locals: ip }));
+    });
+  } else {
+    var server = new static.Server(__dirname+"/static/");
+    server.serve(req, res);
+  }
+
+}).listen(8080);
